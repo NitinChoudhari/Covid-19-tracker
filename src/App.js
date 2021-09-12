f931be36f91555a35eabf1b5b4761077ac727a43
nitin <nitinchoudhari444@gmail.com>
2021-09-12 21:20:42 +0530

    covid-19 tracker first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
+import { Homepage } from './Components/HomePage';
+import { NewsPage } from './Components/NewsPage';
+import { VaccinePage} from './Components/VaccinePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={ Homepage }/>
+        <Route path="/covid19/news" component={ NewsPage }/>
+        <Route path="/Covid-19/vaccineSlots" component={ VaccinePage }/>
+      </Switch>
+    </Router>
   );
 }
 
